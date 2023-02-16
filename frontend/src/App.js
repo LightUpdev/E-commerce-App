48ab4b14a56fe26883b1982bd86a69f5a0f82331
LightUpdev <stevedevop95@gmail.com>
2023-02-16 12:07:41 +0100

    updated code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,49 @@
-import './App.css';
-import { BrowserRouter , Route } from 'react-router-dom';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
+import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
-  const openMenu =()=>{
-    document.querySelector('.sidebar').classList.add('open');
-  }
-  const closeMenu =()=>{
-    document.querySelector('.sidebar').classList.remove('open');
-  }
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
   return (
     <BrowserRouter>
-    <div className="div-container">
-    <header className="header">
-        <div className="brand">
-            <button onClick={openMenu}>
-                &#9776;
-            </button>
-            <a href="index.html">Amazona</a>
-        </div>
-        <div className="header-links">
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
-        </div>
-    </header>
-    <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button onClick={closeMenu}>&times;</button>
-        <ul>
-          <li><a href="index.html">Pants</a></li>  
-          <li><a href="index.html">Shirts</a></li>  
-        </ul>
-    </aside>
-    <main className="main" onClick={closeMenu}>
-        <div className="content">
-        <Route path='/product/:id' exact={true} component={ProductScreen} /> 
-          <Route path='/' exact={true} component={HomeScreen} /> 
-
-       
-    </div>
-    </main>
-    <footer className="footer"> All right reserved</footer>
-</div>
-</BrowserRouter>
+      <div className="div-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Amazona</Link>
+          </div>
+          <div className="header-links">
+            <Link to="/cart">Cart</Link>
+            <Link to="/sign-in">Sign In</Link>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button onClick={closeMenu}>&times;</button>
+          <ul>
+            <li>
+              <Link to="index.html">Pants</Link>
+            </li>
+            <li>
+              <Link to="index.html">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="main" onClick={closeMenu}>
+          <div className="content">
+            <Route path="/product/:id" exact={true} component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer"> All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,8 +1,26 @@
-import React from "react";
-import products from "../data";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  // PRODUCTS FETCHING FROM BACKEND SERVER
+  const fetchProducts = () => {
+    axios
+      .get("api/products")
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <ul className="products">
       {products.map((product) => {
@@ -11,7 +29,6 @@ const HomeScreen = () => {
           <li key={id}>
             <div className="product">
               <Link to={`/product/${id}`}>
-                {" "}
                 <img className="product-image" src={image} alt={name} />
               </Link>
               <div className="product-name">
