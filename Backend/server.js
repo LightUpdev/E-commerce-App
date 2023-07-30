a9b50b1e73beb8a3537da3bda87af7bb05c942f2
light_dev <dgr8steve95@gmail.com>
2023-07-30 10:28:48 +0100

    Changes made

--- Backend/server.js
+++ Backend/server.js
@@ -4,13 +4,7 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
 const app = express();
-app.use(
-  cors({
-    origin: ["https://light-dev-ecommerce.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.get("/api/products", (req, res) => {
   res.status(200).send(data);
