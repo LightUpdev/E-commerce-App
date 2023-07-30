291cc2b139d5b4c62a14e4678096679bd51cda8d
light_dev <dgr8steve95@gmail.com>
2023-07-30 10:22:16 +0100

    Changes made

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const data = require("./data");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: ["https://light-dev-ecommerce.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.get("/api/products", (req, res) => {
   res.status(200).send(data);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
